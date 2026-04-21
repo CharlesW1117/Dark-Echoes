2ef28facee262ccf49c46d7b97984cd94809e181
Charles Wade <Charleskwadejr1088@gmail.com>
2026-04-20 21:31:01 -0500

    Almost done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  // state variables
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  return (
+    <div className="app">
+      <h1>Dark Echoes Episodes</h1>
+      <ul>
+        {episodes.map((episode) => (
+          <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+            {episode.title}
+          </li>
+        ))}
+      </ul>
+
+      {selectedEpisode ? (
+        <div className="details">
+          <h2>{selectedEpisode.title}</h2>
+          <p>{selectedEpisode.description}</p>
+        </div>
+      ) : (
+        <p>Select an episode to see details.</p>
+      )}
+    </div>
+  );
 }
